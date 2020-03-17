c42c57e43996442edbb9640c1cef32e8f2279b87
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-03-17 16:43:08 +0000

    Added room router

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
-  console.log(req.connection.remoteAddress + " has connected");
   res.render('index');
+});
+
+router.get('/room/:id', function(req, res, next)
+{
+  res.render('room', {id: req.params.id});
 });
 
 module.exports = router;
