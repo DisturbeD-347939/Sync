3df88f24184397913f769fe8fe354e4b056b38a6
Ricardo Graça <44160998+DisturbeD-347939@users.noreply.github.com>
2020-03-24 11:26:06 +0000

    Fixed bug

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     $('#createRoom').click(function()
     {
         generateString = Math.random().toString(36).substr(2, 9);
-        var url = "http://localhost:3000/room/" + generateString;
+        var url = window.location.href + "room/" + generateString;
         console.log(url);
         window.location.href = url;
     })
